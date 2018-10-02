5416984cbad610f0cab6e56d021a72857f7d80f0
RXDA <sxfscool@gmail.com>
2018-10-02 11:16:17 +0800

    Backward Elimination 反向淘汰

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_mine.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression_mine.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset=pd.read_csv("D:\\RXDA\\MachineLearningAtoZ\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\50_Startups.csv")
+dataset=pd.read_csv("50_Startups.csv")
 x=dataset.iloc[:, :-1].values
 y=dataset.iloc[:,4].values
 
@@ -22,7 +22,7 @@ onehotencoder = OneHotEncoder(categorical_features=[3])
 x = onehotencoder.fit_transform(x).toarray()
 
 # 虚拟变量陷阱
-# x = x[:,1:]
+x = x[:,1:]
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.2,random_state=0)
@@ -34,6 +34,32 @@ regressor.fit(x_train,y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(x_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+x_train = np.append(arr=np.ones((40,1)), values = x_train, axis=1)
+
+x_opt = x_train[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train ,exog = x_opt).fit()
+regressor_OLS.summary()
+
+x_opt = x_train[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train ,exog = x_opt).fit()
+regressor_OLS.summary()
+
+x_opt = x_train[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train ,exog = x_opt).fit()
+regressor_OLS.summary()
+
+x_opt = x_train[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y_train ,exog = x_opt).fit()
+regressor_OLS.summary()
+
+x_opt = x_train[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y_train ,exog = x_opt).fit()
+regressor_OLS.summary()
+
+
 
 
 
